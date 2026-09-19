105304cc19b09e6e005e386a2983cbbb1d12ceac
njtank <106280943+njtank@users.noreply.github.com>
2026-09-19 07:25:50 -0400

    feat(avid): add Avid Logistics consumable items

--- data/items.lua
+++ data/items.lua
@@ -104,6 +104,87 @@ return {
 		},
 	},
 
+	['sandwich'] = {
+		label = 'Deli Sandwich',
+		weight = 250,
+		client = {
+			image = 'sandwich.png',
+			status = { hunger = 250000 },
+			anim = 'eating',
+			usetime = 2500,
+			cancel = true,
+			notification = 'You ate a deli sandwich'
+		}
+	},
+
+	['donut'] = {
+		label = 'Donut',
+		weight = 150,
+		client = {
+			image = 'donut.png',
+			status = { hunger = 150000 },
+			anim = 'eating',
+			usetime = 2000,
+			cancel = true,
+			notification = 'You ate a donut'
+		}
+	},
+
+	['chocolate_bar'] = {
+		label = 'Chocolate Bar',
+		weight = 100,
+		client = {
+			image = 'chocolate_bar.png',
+			status = { hunger = 120000 },
+			anim = 'eating',
+			usetime = 1800,
+			cancel = true,
+			notification = 'You ate a chocolate bar'
+		}
+	},
+
+	['chips'] = {
+		label = 'Bag of Chips',
+		weight = 150,
+		client = {
+			image = 'chips.png',
+			status = { hunger = 120000 },
+			anim = 'eating',
+			usetime = 2000,
+			cancel = true,
+			notification = 'You ate some chips'
+		}
+	},
+
+	['energy_drink'] = {
+		label = 'Energy Drink',
+		weight = 350,
+		client = {
+			image = 'energy_drink.png',
+			status = { thirst = 200000 },
+			anim = { dict = 'mp_player_intdrink', clip = 'loop_bottle' },
+			prop = { model = `prop_ld_can_01`, pos = vec3(0.01, 0.01, 0.06), rot = vec3(5.0, 5.0, -180.5) },
+			usetime = 2500,
+			cancel = true,
+			notification = 'You drank an energy drink'
+		}
+	},
+
+	['cigarettes'] = {
+		label = 'Cigarettes',
+		weight = 200,
+		consume = 0.05,
+		client = {
+			image = 'cigarettes.png',
+			status = { stress = -100000 },
+			anim = { dict = 'amb@world_human_smoking@male@male_a@enter', clip = 'enter' },
+			prop = { model = `prop_cs_ciggy_01`, pos = vec3(0.015, 0.0, 0.0), rot = vec3(0.0, 0.0, 0.0) },
+			usetime = 2500,
+			cancel = true,
+			notification = 'You smoked a cigarette'
+		}
+	},
+
 	['sprunk'] = {
 		label = 'Sprunk',
 		weight = 350,
